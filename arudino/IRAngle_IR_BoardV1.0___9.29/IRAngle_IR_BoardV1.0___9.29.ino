c9ce5e3d23f92de0ef54e91f6865bbc33405daf0
taiyo kishi <mizunoetaiyo@gmail.com>
2024-01-18 16:57:20 +0900

    bord上でdegitalread高速化+pluseIn

---
+++ arudino/IRAngle_IR_BoardV1.0___9.29/IRAngle_IR_BoardV1.0___9.29.ino
@@ -0,0 +1,70 @@
+
+uint16_t Direction[16] ;
+uint16_t distance;
+uint8_t const IRpin[] = {3, 2, 1, 0, 23, 22, 21, 20, 13, 12, 11, 8, 7, 6, 5, 4};
+float Angle[] = { 0, PI / 8, PI / 4, 3 * PI / 8 , PI / 2, 5 * PI / 8, 3 * PI / 4, 7 * PI / 8,PI, -7 * PI / 8, -3 * PI / 4, -5 * PI / 8, -PI / 2, -3 * PI / 8, -PI / 4, -PI / 8};
+double IRAngle;
+double X;
+double Y;
+double COS[16];
+double SIN[16];
+uint16_t IRport[16];
+uint8_t IRdr;
+int16_t alldata;
+void IRread(int port);
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+
+  for (int i = 0; i < 16; i++) {
+
+    IRread(i);
+
+
+    Direction[i] = IRport[i];
+  }
+
+
+
+  X = 0;
+  Y = 0;
+  for (int i = 0; i < 16; i++) {
+    COS[i] = cos(Angle[i]);
+    SIN[i] = sin(Angle[i]);
+
+    X = X + COS[i] * Direction[i];
+    Y = Y + SIN[i] * Direction[i];
+  }
+  distance = sqrt(X * X + Y * Y);
+  IRAngle = (atan2(Y, X) * 180 / PI) ;
+  IRAngle = round(IRAngle);
+  IRdr = distance * 0.4;
+  if (IRdr > 99) {
+    IRdr = 99;
+  }
+  if (IRAngle > 0) {
+    alldata = IRAngle * 100 + IRdr;
+  } else {
+    alldata = IRAngle * 100 - IRdr;
+  }
+  sendIRData(alldata);
+
+}
+
+void IRread(int port) {
+  IRport[port] = 0 ;
+  for (int i = 0; i < 128; i++) {
+    if (digitalRead(IRpin[port]) == LOW) {
+      IRport[port] = IRport[port] + 1 ;
+
+    }
+}
+}
+
+void sendIRData(int value) {
+  Serial.write('H'); // ヘッダの送信
+  Serial.write(lowByte(value)); // 下位バイトの送信
+  Serial.write(highByte(value)); // 上位バイトの送信
+}
--- arudino/plusInbymyself/plusInbymyself.ino
+++ arudino/plusInbymyself/plusInbymyself.ino
@@ -1,23 +1,25 @@
 uint32_t pulsein(uint8_t pin, uint8_t state, uint16_t timeout);
+#define HIGH 0x4
+#define LOW  0x0
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  Serial.println(pulsein(2,LOW,1000));
+  Serial.println(pulsein(2,HIGH,1000));157
 }
 uint32_t pulsein(uint8_t pin, uint8_t state, uint16_t timeout) {
   uint32_t width = 0;
   uint32_t numloops = 0;
   uint32_t maxloops = timeout;
-  while (digitalRead(pin) == state)
+  while ((PIND & _BV(pin)) == state)
     if (numloops++ > maxloops)
       return 0;
   // numloops = 0;
   // while (digitalRead(pin) != state)
   //   if (numloops++ > maxloops)
   //     return 0;
-  while (digitalRead(pin) != state)
+  while ((PIND & _BV(pin)) != state)
     width++;
   return width;
 }
